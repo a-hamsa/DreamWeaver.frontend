f40d485719797daa133f094fa4ca991fed045097
Anastasia <ahmadhamsa01@gmail.com>
2025-02-23 15:37:43 +0700

    Initialize frontend project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className='bg-gray-800 h-screen flex items-center justify-center'>
+          <h1 className='text-4xl text-red-600'>
+            Hello World
+          </h1>
+        </div>
+    </>
+  )
+}
+
+export default App
